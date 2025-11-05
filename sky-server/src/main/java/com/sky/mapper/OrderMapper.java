a815e77d2b8d5bc511e7bc7265b12059baa605f3
ce <3457187157@qq.com>
2025-11-05 10:09:13 +0800

    查询订单详情业务代码

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -21,15 +21,23 @@ public interface OrderMapper {
     @Select("select * from orders where number = #{orderNumber}")
     Orders getByNumber(String orderNumber);
 
+    void update(Orders orders);
     /**
      * 修改订单信息
      * @param orders
      */
-    void update(Orders orders);
 
     /**
      * 分页条件查询并按下单时间排序
      * @param ordersPageQueryDTO
      */
     Page<Orders> pageQuery(OrdersPageQueryDTO ordersPageQueryDTO);
+
+    /**
+     * 根据id查询订单
+     * @param id
+     */
+    @Select("select * from orders where id=#{id}")
+    Orders getById(Long id);
+
 }
