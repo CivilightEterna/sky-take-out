122b655c4f22e7bde41204aed3a830895d648654
ce <3457187157@qq.com>
2025-11-08 12:15:15 +0800

    订单状态定时处理

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -8,6 +8,7 @@ import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Mapper
 public interface OrderMapper {
@@ -52,4 +53,13 @@ public interface OrderMapper {
 
     @Update("update orders set status = #{orderStatus}, pay_status = #{orderPaidStatus}, checkout_time = #{check_out_time} where number = #{orderNumber}")
     void updateStatus(Integer orderStatus, Integer orderPaidStatus, LocalDateTime check_out_time, String orderNumber);
+
+    /**
+     * 根据状态和下单时间查询订单
+     * @param status
+     * @param orderTime
+     * @return
+     */
+    @Select("select * from orders where status = #{status} and order_time < #{orderTime}")
+    List<Orders> getByStatusAndOrderTimeLT(Integer status, LocalDateTime orderTime);
 }
---
+++ sky-server/src/main/java/com/sky/task/MyTask.java
@@ -0,0 +1,23 @@
+package com.sky.task;
+
+
+
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.stereotype.Component;
+//
+//import java.util.Date;
+//
+//
+//@Component
+//@Slf4j
+///**
+// * 自定义定时任务类
+// */
+//public class MyTask {
+//    @Scheduled(cron = "0/5 * * * * ?")
+//    public void task1() {
+//        log.info("定时任务1开始执行{}", new Date());
+//    }
+//
+//}
